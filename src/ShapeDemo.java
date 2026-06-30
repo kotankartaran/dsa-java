a298d725183c4afb9613ab56ce7907fa39fe5b95
kotankartaran <kotankartaran@gmail.com>
2026-07-01 04:24:30 +0530

    Add shape opp example

---
+++ src/ShapeDemo.java
@@ -0,0 +1,40 @@
+class Shape{
+    public double area(){
+        return 0;
+    }
+}
+class Circle extends Shape{
+    private double radius;
+    public Circle (double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+class Rectangle extends Shape{
+    private double length;
+    private double width;
+    public Rectangle(double length,double width){
+        this.length = length;
+        this.width = width;
+
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+}
+
+public class ShapeDemo {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(5,6);
+        System.out.println("circle area " + circle.area());
+        System.out.println("rectangle area " + rectangle.area());
+
+    }
+}
